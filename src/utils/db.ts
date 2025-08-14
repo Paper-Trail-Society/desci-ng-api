f86ec782737fa6733c1aa82135b97a2d024970ac
Justin Irabor <justin.irabor@iu.org>
2025-08-14 21:47:24 +0100

    conn to dev db supabase successful

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,6 +2,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const client = postgres(process.env.DATABASE_URL!, {
-  pass: process.env.DATABASE_KEY,
+  prepare: false,
 });
-export const db = drizzle({ client });
+export const db = drizzle(client);
