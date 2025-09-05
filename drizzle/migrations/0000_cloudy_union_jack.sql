daa28317c18b7e9c9e121adee210043889af781b
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-09-05 19:52:33 +0100

    Restart database migration history

---
+++ drizzle/migrations/0000_cloudy_union_jack.sql
@@ -0,0 +1,107 @@
+CREATE TABLE "account" (
+	"id" text PRIMARY KEY NOT NULL,
+	"account_id" text NOT NULL,
+	"provider_id" text NOT NULL,
+	"user_id" text NOT NULL,
+	"access_token" text,
+	"refresh_token" text,
+	"id_token" text,
+	"access_token_expires_at" timestamp,
+	"refresh_token_expires_at" timestamp,
+	"scope" text,
+	"password" text,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "categories" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"field_id" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "fields" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"content" text NOT NULL,
+	"abstract" text NOT NULL,
+	"user_id" text,
+	"name" varchar(100) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "keywords" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar NOT NULL,
+	"aliases" jsonb
+);
+--> statement-breakpoint
+CREATE TABLE "paper_keywords" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"paper_id" integer NOT NULL,
+	"keyword_id" integer NOT NULL,
+	CONSTRAINT "paper_keywords_unique_idx" UNIQUE("paper_id","keyword_id")
+);
+--> statement-breakpoint
+CREATE TABLE "papers" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"notes" text NOT NULL,
+	"abstract" text NOT NULL,
+	"user_id" text NOT NULL,
+	"category_id" integer NOT NULL,
+	"ipfs_cid" varchar(80) NOT NULL,
+	"ipfs_url" varchar(255) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "session" (
+	"id" text PRIMARY KEY NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"token" text NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	"ip_address" text,
+	"user_agent" text,
+	"user_id" text NOT NULL,
+	CONSTRAINT "session_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"age" integer,
+	"email" text NOT NULL,
+	"email_verified" boolean NOT NULL,
+	"image" text,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "verification" (
+	"id" text PRIMARY KEY NOT NULL,
+	"identifier" text NOT NULL,
+	"value" text NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"created_at" timestamp,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "categories" ADD CONSTRAINT "categories_field_id_fields_id_fk" FOREIGN KEY ("field_id") REFERENCES "public"."fields"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "paper_keywords" ADD CONSTRAINT "paper_keywords_paper_id_papers_id_fk" FOREIGN KEY ("paper_id") REFERENCES "public"."papers"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "paper_keywords" ADD CONSTRAINT "paper_keywords_keyword_id_keywords_id_fk" FOREIGN KEY ("keyword_id") REFERENCES "public"."keywords"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "papers" ADD CONSTRAINT "papers_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "papers" ADD CONSTRAINT "papers_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "keywords_name_trgm_idx" ON "keywords" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX "keywords_aliases_trgm_idx" ON "keywords" USING gin ((jsonb_path_query_array("aliases", '$[*]')::text) gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX "title_idx" ON "papers" USING btree ("title");--> statement-breakpoint
+CREATE INDEX "abstract_idx" ON "papers" USING btree ("abstract");--> statement-breakpoint
+CREATE INDEX "category_id_idx" ON "papers" USING btree ("category_id");--> statement-breakpoint
+CREATE INDEX "user_id_idx" ON "papers" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "search_index" ON "papers" USING gin ((
+        setweight(to_tsvector('english', "title"), 'A') ||
+        setweight(to_tsvector('english', "abstract"), 'B')
+    ));
