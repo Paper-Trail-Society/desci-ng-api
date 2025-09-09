f40533149c2e1aebc599fbf429f5f1a458134f57
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-09-09 13:29:43 +0100

    Implement multiple authors paper indexing

--- src/scripts/index-papers.ts
+++ src/scripts/index-papers.ts
@@ -28,6 +28,89 @@ const generateRandomPassword = () => {
   return password;
 };
 
+const indexPaper = async (row: CsvObjectType) => {
+  const [category] = await db
+    .select()
+    .from(categoriesTable)
+    .where(
+      eq(categoriesTable.name, row.category.trim().replace(/[^\w\s]/gi, "")) // remove leading or trailing escape characters
+    );
+
+  console.log({ category, categoryName: row.category.trim().replace(/[^\w\s]/gi, "") });
+
+  // ensure institution exists
+  let [institution] = await db
+    .select()
+    .from(institutionsTable)
+    .where(eq(institutionsTable.name, row.category.trim().replace(/[^\w\s]/gi, "")));
+
+  if (!institution) {
+    [institution] = await db
+      .insert(institutionsTable)
+      .values({ name: row.institution.trim().replace(/[^\w\s]/gi, "") })
+      .returning();
+  }
+  console.log({ institution });
+  const abstract = "Dummy abstract";
+
+  const fileResponse = await fetch(row.github_url);
+  const fileBlob = new Blob([await fileResponse.arrayBuffer()]);
+
+  const ipfsResponse = await ipfsService.uploadFile(
+    new File([fileBlob], row.paper_name.split(" ").join("-"), {
+      type: "application/pdf",
+    })
+  );
+
+  console.log({ ipfsResponse });
+
+  const paperCreationPayload = {
+    title: row.paper_name.trim(),
+    abstract,
+    notes: "Dummy note",
+    categoryId: category.id,
+    ipfsCid: ipfsResponse.cid,
+    ipfsUrl: `https://${process.env.PINATA_GATEWAY}/ipfs/${ipfsResponse.cid}`,
+    institutionId: institution.id,
+  };
+
+  console.log({ paperCreationPayload });
+
+  const signUpPayload = {
+    name: row.author.trim(),
+    email: row.email.trim(),
+    institutionId: institution.id,
+    areasOfInterest: [],
+    password: generateRandomPassword(),
+  };
+
+  console.log({ signUpPayload });
+
+  const signUpResponse = await fetch(
+    `${process.env.BETTER_AUTH_URL}/auth/sign-up/email`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signUpPayload),
+    }
+  );
+
+  const user = await signUpResponse.json();
+
+  if (signUpResponse.ok) {
+    console.log({ user });
+
+    await db
+      .insert(papersTable)
+      .values({ ...paperCreationPayload, userId: user.user.id as string })
+      .execute();
+
+    console.log(`📄 Inserted paper: ${row.paper_name}`);
+  }
+};
+
 async function parseCsvAndInsert(filePath: string) {
   const singleAuthorResults: CsvObjectType[] = [];
   const multipleAuthorResults: CsvObjectType[] = [];
@@ -56,88 +139,21 @@ async function parseCsvAndInsert(filePath: string) {
     if (!row.email) continue;
     console.log({ row });
     // ensure category exists
-    const [category] = await db
-      .select()
-      .from(categoriesTable)
-      .where(eq(categoriesTable.name, row.category));
 
-    console.log({ category });
+    await indexPaper(row);
+  }
 
-    // ensure institution exists
-    let [institution] = await db
-      .select()
-      .from(institutionsTable)
-      .where(eq(institutionsTable.name, row.institution));
+  for (const row of multipleAuthorResults) {
+    if (!row.email) continue;
+    console.log({ row });
 
-    if (!institution) {
-      [institution] = await db
-        .insert(institutionsTable)
-        .values({ name: row.institution.trim() })
-        .returning();
-    }
-    console.log({ institution });
-    const abstract = "Dummy abstract";
+    // remove leading and trailing asterisks from the author name
+    const authorName = row.author.split(',')[0].replace(/^\*|\*$/g, '').trim();
+    row.author = authorName;
 
-    const fileResponse = await fetch(row.github_url);
-    const fileBlob = new Blob([await fileResponse.arrayBuffer()]);
-
-    const ipfsResponse = await ipfsService.uploadFile(
-      new File([fileBlob], row.paper_name.split(" ").join("-"), {
-        type: "application/pdf",
-      })
-    );
-
-    console.log({ ipfsResponse });
-
-    const paperCreationPayload = {
-      title: row.paper_name.trim(),
-      abstract,
-      notes: "Dummy note",
-      categoryId: category.id,
-      ipfsCid: ipfsResponse.cid,
-      ipfsUrl: `https://${process.env.PINATA_GATEWAY}/ipfs/${ipfsResponse.cid}`,
-      institutionId: institution.id,
-    };
-
-    console.log({ paperCreationPayload });
-
-    const signUpPayload = {
-      name: row.author.trim(),
-      email: row.email.trim(),
-      institutionId: institution.id,
-      areasOfInterest: [],
-      password: generateRandomPassword(),
-    };
-
-    console.log({ signUpPayload });
-
-    const signUpResponse = await fetch(
-      `${process.env.BETTER_AUTH_URL}/auth/sign-up/email`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(signUpPayload),
-      }
-    );
-
-    const user = await signUpResponse.json();
-
-    if (signUpResponse.ok) {
-      console.log({ user });
-
-      await db
-        .insert(papersTable)
-        .values({ ...paperCreationPayload, userId: user.user.id as string })
-        .execute();
-
-      console.log(`📄 Inserted paper: ${row.paper_name}`);
-    }
+    await indexPaper(row);
   }
 }
-
-// TODO: handle `multipleAuthorResults` similarly
 
 (async () => {
   await parseCsvAndInsert("./desci-ng-fields-categories-authors.csv");
