142d12578ed0b50a5d9691bae3172d98e659d19b
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-09-22 22:23:38 +0100

    Add deleteFilesByCid method to IpfsService

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,23 +1,27 @@
 import { PinataSDK } from "pinata";
 
 class IpfsService {
-    private readonly pinata: PinataSDK;
+  private readonly pinata: PinataSDK;
 
-    constructor() {
-        if (!process.env.PINATA_JWT || !process.env.PINATA_GATEWAY) {
-            throw new Error('[PINATA_JWT] and [PINATA_GATEWAY] is not defined.')
-        }
-        this.pinata = new PinataSDK({
-            pinataJwt: process.env.PINATA_JWT,
-            pinataGateway: process.env.PINATA_GATEWAY,
-        });
+  constructor() {
+    if (!process.env.PINATA_JWT || !process.env.PINATA_GATEWAY) {
+      throw new Error("[PINATA_JWT] and [PINATA_GATEWAY] is not defined.");
     }
+    this.pinata = new PinataSDK({
+      pinataJwt: process.env.PINATA_JWT,
+      pinataGateway: process.env.PINATA_GATEWAY,
+    });
+  }
 
-    async uploadFile(file: File) {
-        const response = await this.pinata.upload.public.file(file);
-        return response;
-    }
-    
+  async uploadFile(file: File) {
+    const response = await this.pinata.upload.public.file(file);
+    return response;
+  }
+
+  async deleteFilesByCid(cids: string[]) {
+    const response = await this.pinata.files.public.delete(cids);
+    return response;
+  }
 }
 
 export const ipfsService = new IpfsService();
