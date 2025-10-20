6e036ff2770964a839b478653f42d1dffcf17a83
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-10-20 16:18:51 +0100

    Allow paper filtering by status for authenticated users and admins
    * Before now, only admins can filter papers by status

--- src/modules/papers/controller.ts
+++ src/modules/papers/controller.ts
@@ -194,14 +194,14 @@ export class PapersController {
       .$dynamic();
 
     const DEFAULT_VIEWABLE_PAPER_STATUS = "published";
-    if (!req.admin) {
+    if (!req.user) {
       baseQuery = baseQuery.where(
         eq(papersTable.status, DEFAULT_VIEWABLE_PAPER_STATUS),
       );
       countQuery = countQuery.where(
         eq(papersTable.status, DEFAULT_VIEWABLE_PAPER_STATUS),
       );
-    } else if (status && req.admin) {
+    } else if (status && req.user) {
       baseQuery = baseQuery.where(eq(papersTable.status, status));
       countQuery = countQuery.where(eq(papersTable.status, status));
     }
