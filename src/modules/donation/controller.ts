effe7a008381b6a7ff2057f5680789ba05c81f11
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2026-01-28 10:05:06 +0100

    Implement paystack donations recording

--- src/modules/donation/controller.ts
+++ src/modules/donation/controller.ts
@@ -5,6 +5,8 @@ import { paystackEventSchema } from "./schema";
 import { db } from "../../config/db";
 import { paystackDonationsTable, usersTable } from "../../db/schema";
 import { eq } from "drizzle-orm";
+import { isWebhookEventFromPaystack } from "./service";
+import ApiError from "../../utils/api-error";
 
 export class DonationController {
   private paystackSecretKey: string;
@@ -19,14 +21,18 @@ export class DonationController {
     this.paystackSecretKey = process.env.PAYSTACK_SECRET_KEY;
     this.donationRepository = donationRepository;
   }
+
   public handlePaystackWebhook = async (req: Request, res: Response) => {
-    // only handle 'charge.success' events
-    // verify that the event is from paystack using the paystack signature || whitelist only paystack IPs for this endpoint
-    const hash = crypto
-      .createHmac("sha512", this.paystackSecretKey)
-      .update(JSON.stringify(req.body))
-      .digest("hex");
-    if (hash != req.headers["x-paystack-signature"]) {
+    const payload = paystackEventSchema.parse(req.body);
+    req.ctx.set("payload", payload);
+
+    if (
+      !isWebhookEventFromPaystack({
+        payload: req.body,
+        paystackSecretKey: this.paystackSecretKey,
+        paystackSignature: req.headers["x-paystack-signature"] as string,
+      })
+    ) {
       // discard request
       req.log.warn(
         "Webhook event doesn't originate from Paystack. Ignoring...",
@@ -34,36 +40,32 @@ export class DonationController {
       return res.status(400).json({ message: "Invalid request" });
     }
 
-    const payload = paystackEventSchema.parse(req.body);
-
-    // confirm that a payment with the paystack reference from the payload doesn't exist in the DB before
     const donationHasExistingRecord =
       await this.donationRepository.doesPaystackDonationWithPaymentRefExist(
         payload.data.reference,
       );
 
     if (donationHasExistingRecord) {
+      req.log.warn(
+        `Donation with payment reference: ${payload.data.reference} has an existing record in the DB.`,
+      );
       return res.status(200).json({ message: "ack" });
     }
 
-    // creating the payment record
-    const paystackDonation = await db.transaction(async (tx) => {
-      const existingUser = await tx
-        .select({ id: usersTable.id })
-        .from(usersTable)
-        .where(eq(usersTable.email, payload.data.customer.email.trim()));
-      const paymentInfo = payload.data;
+    try {
+      await this.donationRepository.createPaystackDonation(payload.data);
+    } catch (error) {
+      req.log.error(
+        error,
+        "An error occurred while creating paystack donation",
+      );
 
-      // tx.insert(paystackDonationsTable).values({
-      //   donorId: existingUser.length > 0 ? existingUser[0].id : null,
-      //   donorEmail: paymentInfo.customer.email,
-      // });
-    });
-    //
-    // check if a user with email of the donor exists in the `users` table. If it exists, user the user ID as the donor ID
-
-    req.ctx.set("payload", req.body);
-
+      throw new ApiError(
+        "An error occurred while creating paystack donation",
+        500,
+        (error as Error).stack,
+      );
+    }
     return res.status(200).json({ message: "ack" });
   };
 }
