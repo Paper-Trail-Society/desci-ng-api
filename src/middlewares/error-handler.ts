9a1eefdcaa6ed6d5a7bc14c9f13edac929062d45
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2026-02-10 15:57:23 +0100

    Use ApiError code is error caught is instanceof ApiError

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,17 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import ApiError from "../utils/api-error";
 
 const ERROR_HANDLER_LOG_ORIGIN = "error-handler-middleware";
 const errorHandler = (
   err: Error,
   req: Request,
-  res: Response,
+res: Response,
   next: NextFunction,
 ) => {
   req.log
     .child({ origin: ERROR_HANDLER_LOG_ORIGIN })
     .error(err, "Internal server error");
-  return res.status(500).send({
-    message: `Internal server error: ${err.message}`,
+  const responseCode = err instanceof ApiError ? err.code : 500;
+
+  return res.status(responseCode).send({
+    error: `Internal server error: ${err.message}`,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
