7e6e6173167542b38449e3b2105e14d1652bd0f8
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2026-04-06 16:16:47 +0100

    chore(tests): Run tests synchronously to ensure DB state consistency across test runs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
+    fileParallelism: false, // running tests synchronously to avoid database inconsistencies across multiple tests during setup/teardown
   },
 });
